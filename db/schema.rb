7cb2edff9acd7a44467eb15d5cf57c910716b6db
Wilton Ramos <wrs@cin.ufpe.br>
2024-11-07 10:40:37 -0300

    Allows to seed from seed csv (#8)
    Allows seed from csv.
    
    https://github.com/v1lton/agroforestry_vis/pull/8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_123501) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_06_145251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_27_123501) do
     t.string "scientific_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["scientific_name"], name: "index_species_on_scientific_name", unique: true
   end
 
   create_table "species_common_names", force: :cascade do |t|
