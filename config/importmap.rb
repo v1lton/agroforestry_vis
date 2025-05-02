e7bef90dd2227c427969a1056a50f2cde25f30e7
Wilton Ramos <wrs@cin.ufpe.br>
2025-05-02 09:35:56 -0300

    Remove Fabric JS from project

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "fabric", to: "https://cdnjs.cloudflare.com/ajax/libs/fabric.js/662/index.min.js" # @6.6.2
 pin "konva", to: "https://unpkg.com/konva@9.3.20/konva.min.js"
